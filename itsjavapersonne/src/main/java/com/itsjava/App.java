6931f5494f095d95112c73e66f2fb5350e555f17
anne-marziale <85136214+anne-marziale@users.noreply.github.com>
2022-09-23 09:57:29 +0200

    app and personne updated

--- itsjavapersonne/src/main/java/com/itsjava/App.java
+++ itsjavapersonne/src/main/java/com/itsjava/App.java
@@ -5,30 +5,35 @@ import java.util.Scanner;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Création d'un ojbet Personne" );
-        Personne p;
-        p = new Personne();
+    public static void main( String[] args ){
+        System.out.println( "Création d'un objet Personne" );
+        Personne p0;
+        p0 = new Personne();
         Personne p1 = new Personne();
-        Personne p2 = new Personne("DOE", "Jhon", new GregorianCalendar(1998,9,8));
+        Personne p2 = new Personne("DOE", "Jhon", new GregorianCalendar(1983,10,8));
 
         Scanner in = new Scanner(System.in, "UTF-8");
         System.out.println("Quel est votre nom ? >> ");
         String nom = in.nextLine();
-        System.out.println("Quel est votre prénom ? >> ");
+        System.out.println("Quel est votre prenom ? >> ");
         String prenom = in.nextLine();
-        System.out.println("Quel est votre année de naissance ? >>");
+        System.out.println("Quel est votre année de naiss ? >> ");
         String annee = in.nextLine();
-        System.out.println("Quel est votre mois de naissance ? >>");
+        System.out.println("Quel est votre mois de naiss ? >> ");
         String mois = in.nextLine();
-        System.out.println("Quel est votre jour de naissance ? >>");
+        System.out.println("Quel est votre jour de naiss ? >> ");
         String jour = in.nextLine();
+        in.close();
 
         Personne p3 = new Personne(nom, prenom, new GregorianCalendar(Integer.parseInt(annee), 
                                                                         Integer.parseInt(mois), 
                                                                         Integer.parseInt(jour)));
-        
+
         p3.affichage(true);
+        p0.affichage(true);
+        p1.affichage(true);
+        p2.affichage(true);
+
+        System.out.println(Personne.getNbInstance());
     }
 }
--- itsjavapersonne/src/main/java/com/itsjava/Personne.java
+++ itsjavapersonne/src/main/java/com/itsjava/Personne.java
@@ -1,97 +1,84 @@
 package com.itsjava;
 
 import java.util.GregorianCalendar;
+import java.util.Random;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Personne {
-    private String nom;
-    private String prenom;
-    private GregorianCalendar date_naiss;
+    @Getter
+    String nom;
+    @Getter
+    String prenom;
+    @Getter @Setter
+    GregorianCalendar date_naiss = new GregorianCalendar();
     // variable privée représentant le numéro de la personne
-    private int numPersonne;
-    // variable static privé rerésentant le compteur de création de Personnes
-    private static int nbInstance;
+    @Getter @Setter
+    int numPersonne;
+    // variable static privée représentant le compteur de création de Personne
+    @Getter
+    static int nbInstance;
+    {nbInstance++;}
 
-    // Constructeur simple
-    Personne() {
-        System.out.println("Constructeur Simple");
-        nom = "";
-        prenom = "";
-        date_naiss = null;
-    }
+    // Constructeur simple généré par lombok
 
-    // constructeur surchargé
-    public Personne(String nom, String prenom, GregorianCalendar date_naiss) {
+    // Constructeur surchargé
+    Personne(String nom, String prenom, GregorianCalendar date_naiss) {
         System.out.println("Constructeur surchargé");
-        this.nom = nom;
-        this.prenom = prenom;
-        this.date_naiss = date_naiss;
+        setNom(nom);
+        setPrenom(prenom);
+        setDate_naiss(date_naiss);
+        setNumPersonne(new Random().nextInt(1000));
     }
 
-    // Destructeur 
+    // Destructeur
     protected void finalize() throws Throwable{
-        System.out.println("Destruction de notre objet Personne");
+        System.out.println("Destruction de l'objet Personne");
     }
 
-    // Méthode pour calculer l'âge
+    // Méthode pour calculer l'age
     public long calculAge(){
         long age;
-        date_naiss = new GregorianCalendar(1998, 9, 29);
+        date_naiss = getDate_naiss();
         age = new GregorianCalendar().getTimeInMillis() - date_naiss.getTimeInMillis();
         age = age / 1000 / 60 / 60 / 24 / 365;
         return age;
     }
-
-    // Methode pour afficher des informations de personnes.
-    public void affichage() {
+    // Méthode pour afficher des informations de personne
+    @Deprecated
+    public void affichage(){
         System.out.println("Nom : " + getNom());
         System.out.println("Prénom : " + getPrenom());
         System.out.println("Age : " + calculAge());
+        System.out.println("Numéro client : " + getNumPersonne());
     }
-
-    // Méthode pour afficher des informations et si c'est autre que fr alors on
-    // affiche en anglais
-    public void affichage(boolean fr) {
-        if (fr) {
+    //Méthode pour afficher des informations et si c'est autre que fr alors on affiche en anglais
+    public void affichage(boolean fr){
+        if(fr){
             affichage();
-        } else {
+        }else{
             System.out.println("Last name : " + getNom());
             System.out.println("First name : " + getPrenom());
             System.out.println("Age : " + calculAge());
+            System.out.println("Client number: " + getNumPersonne());
         }
     }
-
-    public String getPrenom() {
-        return this.prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom.substring(0, 1).toUpperCase()
-                + prenom.substring(1).toLowerCase();
-    }
-
-    // Getter Setter afin d'accéder à nos variables déclarées en privées
-    public String getNom() {
-        return this.nom;
-    }
+    // Getter Setter afin d'accéder à nos varibles déclarées en privées
 
     public void setNom(String nom) {
         this.nom = nom.toUpperCase();
     }
 
-    public GregorianCalendar getDate_naiss() {
-        return this.date_naiss;
-    }
-
-    public void setDate_naiss(GregorianCalendar date_naiss) {
-        this.date_naiss = date_naiss;
-    }
-
-    public int getnumPersonne(){
-        return numPersonne;
-    }
-
-    public static int getNbInstance(){
-        return nbInstance;
+    public void setPrenom(String prenom) {
+        this.prenom = prenom.substring(0,1).toUpperCase() 
+                        + prenom.substring(1).toLowerCase();
     }
 
 }
+// fin de la class
